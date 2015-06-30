dfea4d3d88c21ecaf6be11ad35b4c83225f7bcb7
minling <mzhao725@gmail.com>
2015-06-30 09:30:10 -0400

    done

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,2 +1,42 @@
 # create your PigeonsController here
 # it should inherit from ApplicationController
+
+class PigeonsController < ApplicationController
+  get '/pigeons' do
+    @pigeons = Pigeon.all
+     erb :'pigeons/index'
+  end
+
+  get '/pigeons/new' do
+    erb :'pigeons/new'
+  end
+
+  get '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+   erb :'pigeons/show'
+  end
+
+  get '/pigeons/:id/edit' do
+    @pigeon = Pigeon.find(params[:id])
+    erb :'pigeons/edit'
+  end
+
+  post '/pigeons' do
+
+    @pigeon = Pigeon.create(params)
+    redirect '/pigeons'
+  end
+
+   delete '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.destroy
+    redirect '/pigeons'
+  end
+
+  patch '/pigeons/:id' do
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.update(:color => params[:color])
+    redirect "/pigeons/#{params[:id]}"
+  end
+
+end
--- db/migrate/01_create_pigeons.rb
+++ db/migrate/01_create_pigeons.rb
@@ -3,3 +3,13 @@
 # what columns to include
 
 # remember to inherit from the correct ActiveRecord module
+class CreatePigeons < ActiveRecord::Migration
+  def change
+    create_table :pigeons do |t|
+      t.string :name
+      t.string :color
+      t.string :gender
+      t.string :lives 
+    end
+  end
+end
